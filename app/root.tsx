5e3d194d6f5a543a3cdfa66f73732d2343dba14f
Sin Jaeho <stra2003@gmail.com>
2025-04-02 17:19:59 +0900

    reinstall to nico version

--- app/root.tsx
+++ app/root.tsx
@@ -21,11 +21,12 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
